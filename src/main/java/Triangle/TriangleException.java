9105134cde518c8ad1d7453cd5a7f0c9751c341f
ReFeeD <mfeed175@gmail.com>
2022-08-22 18:59:38 +0300

    Lesson4

---
+++ src/main/java/Triangle/TriangleException.java
@@ -0,0 +1,11 @@
+package Triangle;
+
+
+import org.opentest4j.AssertionFailedError;
+
+public class TriangleException extends org.opentest4j.AssertionFailedError{
+    public TriangleException(AssertionFailedError error) {
+        super(error.getMessage());
+
+    }
+}

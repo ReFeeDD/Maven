7450df5eff15a1c42d6c7142eeee1174d2a01ad4
ReFeeD <mfeed175@gmail.com>
2022-09-20 18:23:46 +0300

    Lesson3

---
+++ src/test/java/Lesson3/AbstractTest.java
@@ -0,0 +1,27 @@
+package Lesson3;
+
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public abstract class AbstractTest {
+    static Properties properties = new Properties();
+    static String propertiesLink = "src/main/resources/setup.properties";
+
+    @BeforeAll
+    static void initTest() throws IOException {
+        properties.load(new FileInputStream(propertiesLink));
+
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.baseURI = properties.getProperty("baseUrl");
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+                .addQueryParam("apiKey", properties.getProperty("apiKey"))
+                .build();
+    }
+
+}

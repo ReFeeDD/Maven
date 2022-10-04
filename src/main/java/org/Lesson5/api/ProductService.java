f54c3426f26d709b07ba0bb3b8166f0617b51d73
ReFeeD <mfeed175@gmail.com>
2022-10-04 21:02:51 +0300

    Lesson5

---
+++ src/main/java/org/Lesson5/api/ProductService.java
@@ -0,0 +1,26 @@
+package org.Lesson5.api;
+
+import okhttp3.ResponseBody;
+import org.Lesson5.dto.ProductDto;
+import retrofit2.Call;
+import retrofit2.http.*;
+
+import java.util.List;
+
+public interface ProductService {
+    @POST("products")
+    Call<ProductDto> createProduct(@Body ProductDto productDto);
+
+    @DELETE("products/{id}")
+    Call<ResponseBody> deleteProduct(@Path("id") int id);
+
+    @PUT("products")
+    Call<ProductDto> modifyProduct(@Body ProductDto productDto);
+
+    @GET("products/{id}")
+    Call<ProductDto> getProductByID(@Path("id") int id);
+
+    @GET("products")
+    Call<List<ProductDto>> getProducts();
+}
+
---
+++ src/main/java/org/Lesson5/dto/ProductDto.java
@@ -0,0 +1,20 @@
+package org.Lesson5.dto;
+
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@With
+public class ProductDto {
+
+    private Integer id;
+    private String title;
+    private Integer price;
+    private String categoryTitle;
+
+
+}
+
+
